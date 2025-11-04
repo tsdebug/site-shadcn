39820632b677d8ae6153cc39889cd721dc8dd196
TSdebug <taworkspace01@gmail.com>
2025-11-05 02:44:13 +0530

    Added shadcn-nav1 block

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/sections/Nav";
+import { Navbar1 } from "@/components/navbar1";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Nav />
+        <Navbar1 />
         {children}
       </body>
     </html>
