579c25791cf9710be443d9bdf0a477e5ae6bb080
Tsdebug <taworkspace01@gmail.com>
2025-12-06 20:48:37 +0530

    Added ImageSlider to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { ImgSlider } from "@/components/sections/ImgSlider"
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <div><ImgSlider/></div>
   );
 }
