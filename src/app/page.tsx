e4f73757164a2df5aaab99120b0f9c8e1bd23efa
Tsdebug <taworkspace01@gmail.com>
2025-12-12 00:57:13 +0530

    Added Aceternity UI component - infinite moving cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { ImgSlider } from "@/components/sections/ImgSlider"
+import { Highlights } from "@/components/sections/Highlights"
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
+    <div><Highlights/></div>
     <div><ImgSlider/></div>
+    </>
   );
 }
